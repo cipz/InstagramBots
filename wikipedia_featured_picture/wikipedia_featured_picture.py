3bd2a0abc94435cf503a5f40df04d765974f5e64
Ciprian Voinea <34091723+cipz@users.noreply.github.com>
2020-10-27 10:59:48 +0200

    Credit fix

--- wikipedia_featured_picture/wikipedia_featured_picture.py
+++ wikipedia_featured_picture/wikipedia_featured_picture.py
@@ -57,7 +57,7 @@ photo_credit = ''
 
 if 'Photograph credit:' in full_article_description:
     article_description = full_article_description.split('Photograph credit:')[0].strip()
-    photo_credit = full_article_description.split('Photograph credit:')[1].split('Archive')[0].strip()
+    photo_credit = '\n\nCredit: ' + full_article_description.split('Photograph credit:')[1].split('Archive')[0].strip()
 elif 'Engraving credit:' in full_article_description:
     article_description = full_article_description.split(('Engraving credit:'))[0].strip()
     photo_credit = '\n\nCredit: ' + full_article_description.split('Engraving credit:')[1].split('Archive')[0].strip()
