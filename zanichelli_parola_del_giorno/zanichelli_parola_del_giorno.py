ba6738b05346d4101eea09bed9116ac2f434c2a3
cip <ciprianv96@hotmail.it>
2022-01-05 15:43:58 +0100

    zanichelli small fix

--- zanichelli_parola_del_giorno/zanichelli_parola_del_giorno.py
+++ zanichelli_parola_del_giorno/zanichelli_parola_del_giorno.py
@@ -1,144 +1,174 @@
-from instagrapi import Client
+#!/usr/bin/python
+
+# Importing custom files
+import sys
+sys.path.insert(1, '../')
+
+import utils
+import instagram
+
+from datetime import date
+from PIL import Image, ImageDraw
 from bs4 import BeautifulSoup
-from pdf2image import convert_from_path
+import urllib.request
 import requests
-import wget
 import time
+import wget
 import json
 import os
 
-# Grabbing the username and password for the account
-with open('params.json', 'r') as params_file:
-    json_params = json.load(params_file)
+def main(debug):
+    
+    ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- 
 
-params_file.close()
+    # Getting parameters
+    params_file = 'params.json'
+    params = utils.get_params(params_file, 'zanichelli_username', 'zanichelli_password')
 
-# When testing locally with username and password in params file
-# username = json_params['username']
-# password = json_params['password']
+    username = params['username']
+    password = params['password']
+    previous_post_key = params['previous_post_key']
 
-# When the username and password are stored as environment variables
-username = os.getenv('zanichelli_username')
-password = os.getenv('zanichelli_password')
+    ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- 
 
-previous_post_key = json_params['previous_post_key']
+    # Defining edited parameters dictionary
+    edit_params = {}
 
-print("Setting url and downloading content")
-url = 'https://dizionaripiu.zanichelli.it/cultura-e-attualita/le-parole-del-giorno/parola-del-giorno/'
+    ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- ## -- 
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+    print("Setting url and downloading content")
+    url = 'https://dizionaripiu.zanichelli.it/cultura-e-attualita/le-parole-del-giorno/parola-del-giorno/profundis'
 
-parola_title = soup.find('header', {'class':'mb-4'}).text.strip()
-sillabazione = soup.find('span', {'style':'display: block; margin-left: 00pt; text-indent: -00pt;'}).text.replace('SINONIMI','').replace('SILLABAZIONE:','').strip()
-tipologia_parola = soup.find('span', {'style':'display: block; margin-left: 12pt; text-indent: -06pt;'}).text.strip()
-significati_parola = soup.findAll('span', {'style':'display: block; margin-left: 24pt; text-indent: -06pt;'})
-origine_parola = significati_parola[0].text
-significati_parola = significati_parola[1:]
-data = soup.find('div', {'class':'article-meta col text-right'}).text
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# Checking word
-if previous_post_key != "" and previous_post_key == parola_title:
-    print("\n\n\nThere is nothing new to post\n\nDONE")
-    exit(0)
+    parola_title = soup.find('header', {'class':'mb-4'}).text.strip()
 
-json_params['previous_post_key'] = parola_title
+    # Checking word
+    if previous_post_key != "" and previous_post_key == parola_title:
+        print("\n\n\nThere is nothing new to post\n\nDONE")
+        exit(0)
 
-caption_significati = ''
-for i in significati_parola:
-    if len(caption_significati) > 1600:
-        break
-    caption_significati += '\n' + i.text
+    # Setting the previous post key
+    edit_params['previous_post_key'] = parola_title
 
-# Setting caption
-print("Setting caption")
-hashtags = '#' + parola_title + ' #zanichelli #dizionario #paroladelgiorno #grammatica #cultura #italia #linguaitaliana'
-caption = 'La parola del giorno è: ' + parola_title + '\n' + caption_significati + '\n\n' + data + '\n\n' + hashtags
 
-print("Caption:\n\n")
-print(caption)
-print()
+    main_div = soup.find('div', {'class': 'main-content light-txt'})
+    
+    sillabazione = main_div.find('span', {'style': 'display: inline; font-style: normal; font-weight: 300; font-size: 1em; color: #C53329; font-family: "Noto Sans", Verdana, Georgia, Tahoma, sans-serif !important;'}).text.strip()
+    tipologia_parola = main_div.find('span', {'style':'display: inline; font-style: normal; font-weight: 600; font-size: 1em; color: #444; font-family: "Noto Sans", Verdana, Georgia, Tahoma, sans-serif !important;'}).text.strip()
 
-print("\n\nSetting correct values in tex file")
-words_file = open('latex/words.txt', 'r+')
-words_file.truncate(0)
+    info_parola = main_div.findAll('span', {'style':'display: inline; font-style: normal; font-weight: 300; font-size: 1em; color: #444; font-family: "Noto Sans", Verdana, Georgia, Tahoma, sans-serif !important;'})
+    
+    pronuncia_parola = info_parola[0].text
+    origine_parola = info_parola[1].text
 
-# A long word just to test
-# parola_title = 'Supercalifragilistichespiralidoso'
+    significati_parola = main_div.findAll('div', {'style': 'display: block; background-color: #FFFFFF; margin-top: 1em; margin-left: 0em; padding-bottom: 0.9em; border-bottom-style: solid; border-bottom-color: #ccc; border-bottom-width: 1px;'})
 
-# If the word is too long and does not fit the picture
-len_parola_title = len(parola_title)
-# print(len_parola_title)
-if len_parola_title > 30:
-    parola_title = '\\footnotesize ' + parola_title
+    caption_significati = ''
 
-elif len_parola_title > 19:
-    parola_title = '\small ' + parola_title
+    if len(significati_parola) > 1:
+        for significato in significati_parola:
 
-elif len_parola_title > 10:
+            if len(caption_significati) > 1600:
+                break
 
-    if ' / ' in parola_title:
-        parola_title = parola_title.replace(' / ', ' / \\newline ')
-    elif ' ' in parola_title:
-        parola_title = parola_title.replace(' ', '\\newline ')
+            significato_entry = significato.find('p')
+            significato_entry_text = significato_entry.text.strip()
+            significato_entry_text = ' '.join(significato_entry_text.split())
+
+            caption_significati += '\n' + significato_entry_text
     else:
-        parola_title = '\LARGE ' + parola_title
+        caption_significati += '\n' + significati_parola[0].text
 
-line = '\\newcommand{\parolatitle}{' + parola_title + '}'
-words_file.write(line)
+    data = soup.find('div', {'class':'article-meta col text-right'}).text
 
-line = '\\newcommand{\sillabazione}{' + sillabazione + '}'
-words_file.write(line)
+    # Setting caption
+    print("Setting caption")
+    hashtags = '#' + parola_title + ' #zanichelli #dizionario #paroladelgiorno #grammatica #cultura #italia #linguaitaliana'
+    caption = 'La parola del giorno è: ' + parola_title + '\n' + caption_significati + '\n\n' + data + '\n\n' + hashtags
 
-if len(origine_parola) > 60:
-    tipologia_parolas = '{\\scriptsize' + tipologia_parola + '}'
-    origine_parola = '{\\begin{spacing}{0.85}\\noindent\\footnotesize' + origine_parola + '\end{spacing}}'
+    print("Caption:\n\n")
+    print(caption)
+    print()
 
-line = '\\newcommand{\\tipologiaparola}{' + tipologia_parola + '}'
-words_file.write(line)
+    print("\n\nSetting correct values in tex file")
+    words_file = open('latex/words.txt', 'r+')
+    words_file.truncate(0)
 
-line = '\\newcommand{\origineparola}{' + origine_parola + '}'
-words_file.write(line)
+    # A long word just to test
+    # parola_title = 'Supercalifragilistichespiralidoso'
 
-line = '\\newcommand{\data}{' + data + '}'
-words_file.write(line)
+    # If the word is too long and does not fit the picture
+    len_parola_title = len(parola_title)
+    # print(len_parola_title)
+    if len_parola_title > 30:
+        parola_title = '\\footnotesize ' + parola_title
 
-words_file.close()
+    elif len_parola_title > 19:
+        parola_title = '\small ' + parola_title
 
-print("\n\nCompliling tex file")
-# Twice because the first time it may not get the images' position correctly
-os.system("cd latex ; xelatex main.tex >> /dev/null ; xelatex main.tex >> /dev/null")
-# pdflatex is very verbose and useful to test if / where an error occors
-# os.system("cd latex ; xelatex main.tex ; xelatex main.tex")
- 
-# Wait for the pdf to be correctly created
-print('Waiting for the pdf to be correctly created')
-time.sleep(20)
+    elif len_parola_title > 10:
 
-print("\n\nTransforming pdf in jpg")
-pages = convert_from_path('latex/main.pdf', 500)
-pages[0].save('out.jpg', 'JPEG')
+        if ' / ' in parola_title:
+            parola_title = parola_title.replace(' / ', ' / \\newline ')
+        elif ' ' in parola_title:
+            parola_title = parola_title.replace(' ', '\\newline ')
+        else:
+            parola_title = '\LARGE ' + parola_title
 
-# Wait for the img to be converted
-print('Wait for the img to be converted')
-time.sleep(10)
+    line = '\\newcommand{\parolatitle}{' + parola_title + '}'
+    words_file.write(line)
 
-print("\n\nPosting to instagram")
+    line = '\\newcommand{\sillabazione}{' + sillabazione + '}'
+    words_file.write(line)
 
-cl = Client()
-cl.login(username, password)
-cl.photo_upload('out.jpg', caption = caption)
+    if len(origine_parola) > 60:
+        tipologia_parolas = '{\\scriptsize' + tipologia_parola + '}'
+        origine_parola = '{\\begin{spacing}{0.85}\\noindent\\footnotesize' + origine_parola + '\end{spacing}}'
 
-# Removing config folder from instabot
-# os.system('rm "' + str(img_download) + '"')
-os.system('rm img.jpg')
-os.system('rm out.jpg')
-os.system('rm *REMOVE_ME')
-os.system('rm params.json')
+    line = '\\newcommand{\\tipologiaparola}{' + tipologia_parola + '}'
+    words_file.write(line)
 
-with open('params.json', 'w') as new_params_file:
-    json.dump(json_params, new_params_file)
+    line = '\\newcommand{\origineparola}{' + origine_parola + '}'
+    words_file.write(line)
 
-params_file.close()
+    line = '\\newcommand{\data}{' + data + '}'
+    words_file.write(line)
 
-print("\n\nDONE")
+    words_file.close()
+
+    print("\n\nCompliling tex file")
+    # utils.compile_xelatex_verbose()
+    utils.compile_xelatex_silent()
+    
+    print("\n\nTransforming pdf in jpg")
+    utils.pdf_2_jpg('latex/main.pdf', 'out.jpg')
+
+    if not debug:
+
+        print("Posting to instagram")
+        instagram.post_image('out.jpg', full_caption, username, password)
+
+        print('Setting new parameters')
+        utils.set_params(params_file, edit_params)
+    
+        # Removing stuff (not necessary if used in docker container of github actions)
+        # Useful if executed locally
+        print("Removing useless files")
+        os.system('rm *REMOVE_ME')
+        os.system('rm img.jpg')
+        os.system('rm out.jpg')   
+
+if __name__ == "__main__":
+
+    if "--debug" in sys.argv[1:]:
+        debug=True
+    elif "-d" in sys.argv[1:]:
+        debug=True
+    else:
+        debug=False
+
+    main(debug)
+
+    print("\nDONE")
